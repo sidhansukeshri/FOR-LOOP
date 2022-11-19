d20311afcffb3006f9677f091b7ffd91df1e1c6f
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-19 15:03:57 +0530

    Create 10.Check whether a given number is prime or not.c

---
+++ 10.Check whether a given number is prime or not.c
@@ -0,0 +1,17 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+void main(){
+    int num,i,j=0;
+    printf("Input  a number: ");
+    scanf("%d",&num);
+    for(i=2;i<=num/2;i++){
+        if(num % i==0){
+         j++;
+            break;
+        }
+    }
+   if(j==0 && num!= 1)
+        printf("%d is a prime number.\n",num);
+   else
+      printf("%d is not a prime number",num);
+}
