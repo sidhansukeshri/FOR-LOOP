f46f2c134268c41107ebb7f2ec0b5b403efa77f7
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-19 14:37:43 +0530

    Create 4.Read 10 numbers and find their sum and average.c

---
+++ 4.Read 10 numbers and find their sum and average.c
@@ -0,0 +1,16 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+void main()
+{   
+  int i,n,sum=0;
+	float avg;
+	printf("Input the 10 numbers : \n");
+	for (i=1;i<=10;i++)
+	{
+	  printf("Number-%d :",i);
+		scanf("%d",&n);
+		sum +=n;
+	}
+	avg=sum/10.0;
+	printf("The sum of 10 no is : %d\nThe Average is : %f\n",sum,avg);
+}
